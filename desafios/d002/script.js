f630cee0852b163f952ee8ace7dc850e9dd39f52
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-11-09 15:32:56 -0300

    Atualizei os desafios

---
+++ desafios/d002/script.js
@@ -0,0 +1,5 @@
+function botao() {
+    nome = window.prompt("Qual é o seu nome?")
+    idade = window.prompt(`Olá, ${nome}! Quantos anos você tem?`)
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
