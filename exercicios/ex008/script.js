74f8b88529d897018e2195da8b7b3c79e94a0236
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-11-11 19:12:08 -0300

    Att do exercicio 008

--- exercicios/ex008/script.js
+++ exercicios/ex008/script.js
@@ -1,5 +1,5 @@
 function botao() {
     var n1 = Number(window.prompt('Digite um número:'))
     var inteira = n1
-    
+    res.innerHTML = `O número a ser analisado será o <strong>${n1}</strong>`
 }
