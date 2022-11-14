ce861d406e45fcc07862cebecfd53952e1550b3a
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-11-14 11:55:33 -0300

    Fiz o desafio 007

---
+++ desafios/d007/script.js
@@ -0,0 +1,7 @@
+var cotacao = Number(window.prompt('Antes de mais nada. Quanto está a cotação do dólar agora?'))
+
+function  converter() {
+    var carteira = Number(window.prompt('Quantos R$ você tem na carteira?'))
+    var convertido = carteira / cotacao
+    res.innerHTML = `<h2>O valor de R$${carteira.toFixed(2)} corresponde à US$${convertido.toFixed(2)}</h2>`
+}
