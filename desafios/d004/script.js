43ec5f5783d057d057d2cfbc2caaff6f79fe1dee
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-11-14 13:48:06 -0300

    Atualizei alguns desafios

--- desafios/d004/script.js
+++ desafios/d004/script.js
@@ -3,5 +3,5 @@ function botao() {
     var preco = Number(window.prompt(`Quanto custa o ${produto} que você está comprando?`))
     var valor = Number(window.prompt(`Qual foi o valor que você deu para pagar o ${produto}`))
     var troco = valor - preco
-    window.alert(`Você comprou um ${produto} que custou R$${preco},00. Deu R$${valor},00 em dinheiro e vai receber R$${troco},00 de troco. Volte Sempre!`)
+    window.alert(`Você comprou um ${produto} que custou ${preco.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})}. Deu ${valor.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})} em dinheiro e vai receber ${troco.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})} de troco. Volte Sempre!`)
 }
--- desafios/d007/script.js
+++ desafios/d007/script.js
@@ -3,5 +3,5 @@ var cotacao = Number(window.prompt('Antes de mais nada. Quanto está a cotação
 function  converter() {
     var carteira = Number(window.prompt('Quantos R$ você tem na carteira?'))
     var convertido = carteira / cotacao
-    res.innerHTML = `<h2>O valor de ${carteira.toFixed(2)} corresponde à US$${convertido.toFixed(2)}</h2>`
+    res.innerHTML = `<h2>O valor de ${carteira.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})} corresponde à ${convertido.toLocaleString('en', {style: 'currency', currency:'USL'})}</h2>`
 }
