1b10653d56723355054989845e7b63e680d1eee4
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-11-14 10:57:04 -0300

    Fiz o desafio 003

---
+++ desafios/d003/script.js
@@ -0,0 +1,4 @@
+function botao() {
+    var n = Number(window.prompt('Digite um número inteiro qualquer'))
+    window.alert('Antes de ' + n + ', temos o número ' + (n - 1) + '.' + ' Depois de ' + n + ' temos o número ' + (n + 1) + '.')
+}
