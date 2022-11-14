b373eb89dc14281b05bea15a7e5eaadc371a0a4d
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-11-14 13:42:58 -0300

    Atualizei os desafio 008 e 007

--- desafios/d008/script.js
+++ desafios/d008/script.js
@@ -4,7 +4,7 @@ function desconto() {
     var desconto = preco * 0.10
     var preco2 = preco - desconto
     res.innerHTML = `<h2><strong>Calculando o desconto de 10% para ${produto}</strong></h2>`
-    res.innerHTML += `<p>O preço original era R$${preco}.</p>`
-    res.innerHTML += `<p>Você acaba de ganhar R$${desconto.toFixed(2)} de desconto (-10%).</p>`
-    res.innerHTML += `<p>No fim, você vai pagar R$${preco2.toFixed(2)} no produto ${produto}.</p>`
+    res.innerHTML += `<p>O preço original era ${preco.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})}.</p>`
+    res.innerHTML += `<p>Você acaba de ganhar ${desconto.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})} de desconto (-10%).</p>`
+    res.innerHTML += `<p>No fim, você vai pagar ${preco2.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})} no produto ${produto}.</p>`
 }
