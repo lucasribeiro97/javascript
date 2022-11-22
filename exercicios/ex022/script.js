df7e14ef0e459945f53fdcac1a545f4620c0838c
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-11-22 14:10:47 -0300

    Fiz o exercício 022

--- exercicios/ex022/script.js
+++ exercicios/ex022/script.js
@@ -1,7 +1,14 @@
 function contar() {
-    res.innerHTML += `<p>Contando de 1 até 10</p>`
-    for (c = 1; c <= 10; c = c + 1) {
-        res.innerHTML += `${c} \u{1F449}`
+    res.innerHTML += `<p>Contando de 1 até 10, marcandos os pares</p>`
+    let c = 1
+    while (c <= 10) {
+        if (c % 2 == 0) {
+            res.innerHTML += `<mark><strong>${c}</strong></mark>\u{1F449}`
+        } else {
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        c++
     }
+
     res.innerHTML += `\u{1F3C1}`
 }
