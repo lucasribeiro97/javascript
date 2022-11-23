2a2bdbb22e0684e2ff75d802804b4ee50e7dfabc
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-11-23 11:38:59 -0300

    Fiz o exercício 026

--- exercicios/ex026/script.js
+++ exercicios/ex026/script.js
@@ -1,9 +1,25 @@
 function contar() {
-    let num = document.getElementById('txtn')
-    let n = num.value
-    res.innerHTML += `<p>Contando de 0 até ${n}</p>`
-    for (c = 0; c <= n; c = c + 1) {
-        res.innerHTML += `${c} \u{1F449}`
+    let n1 = Number(document.getElementById('txtn1').value)
+    let n2 = Number(document.getElementById('txtn2').value)
+    let c
+
+    res.innerHTML += `<p>Contando de ${n1} até ${n2}</p>`
+
+    if (n1 < n2) {
+        c = n1
+        while (c <= n2) {
+            res.innerHTML += `${c} \u{1F449}`
+            c ++
+        }
+    } else if (n2 < n1) {
+        c = n1
+        while (c >= n2) {
+            res.innerHTML += `${c} \u{1F449}`
+            c --
+        }
+    } else {
+        res.innerHTML += `Não é possível contar, pois o números são iguais`
     }
+
     res.innerHTML += `\u{1F3C1}`
 }
